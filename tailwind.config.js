0be3b61e9efa50061bfedda89e0d775d47135824
arionkosturi <arionkosturi@gmail.com>
2024-04-24 22:25:51 +0200

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*.{html,js}"],  theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'live': "url('https://i.ytimg.com/vi/GsUkBv1hP0s/maxresdefault.jpg')",
+      }
+    },
   },
   plugins: [],
+
   darkMode: 'class',
 }
 
